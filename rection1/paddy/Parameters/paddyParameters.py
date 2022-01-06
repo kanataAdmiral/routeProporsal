664f0f8f96b39f1ae3f57a21d245f77e0dbbf395
kanata_admiral <196009@std.hi-joho.ac.jp>
2022-01-06 14:49:13 +0900

    Revert "ルート検索の情報をJSONにまとめるようにした"
    This reverts commit 51b63ec971ee1562a0921bc47431464fb20b1b82.

--- rection1/paddy/Parameters/paddyParameters.py
+++ rection1/paddy/Parameters/paddyParameters.py
@@ -1,4 +1,5 @@
-from rection1.paddy.machineMovement.moveparameter import *
+
+from ..machineMovement.moveparameter import *
 
 START_POSITION = "S"
 END_POSITION = "E"
@@ -7,50 +8,52 @@ INSIDE_POSITION = "I"
 
 
 class movement:
-    vector: tuple
-    string: str
-    icon: str
-
+    move = None
     def __init__(self, up, left):
+        move = moveParameter
         if up < 0:
             if left < 0:
                 # 左上
-                self.vector = -1, -1
-                self.string = "上左"
-                self.icon = "↖"
+                move.vector = -1, -1
+                move.string = "上左"
+                move.icon = "↖"
             elif left > 0:
                 # 右上
-                self.vector = -1, 1
-                self.string = "上右"
-                self.icon = "↗"
+                move.vector = -1, 1
+                move.string = "上右"
+                move.icon = "↗"
             else:
                 # 上
-                self.vector = -1, 0
-                self.string = "上"
-                self.icon = "↑"
+                move.vector = -1, 0
+                move.string = "上"
+                move.icon = "↑"
         elif up > 0:
             if left < 0:
-                self.vector = 1, -1
-                self.string = "下左"
-                self.icon = "↙"
+                move.vector = 1, -1
+                move.string = "下左"
+                move.icon = "↙"
             elif left > 0:
-                self.vector = 1, 1
-                self.string = "下右"
-                self.icon = "↘"
+                move.vector = 1, 1
+                move.string = "下右"
+                move.icon = "↘"
             else:
-                self.vector = 1, 0
-                self.string = "下"
-                self.icon = "↓"
+                move.vector = 1, 0
+                move.string = "下"
+                move.icon = "↓"
         else:
             if left < 0:
-                self.vector = 0, -1
-                self.string = "左"
-                self.icon = "←"
+                move.vector = 0, -1
+                move.string = "左"
+                move.icon = "←"
             elif left > 0:
-                self.vector = 0, 1
-                self.string = "右"
-                self.icon = "→"
+                move.vector = 0, 1
+                move.string = "右"
+                move.icon = "→"
             else:
-                self.vector = 0, 0
-                self.string = "終点"
-                self.icon = "・"
+                move.vector = 0, 0
+                move.string = "終点"
+                move.icon = "・"
+        self.move = move
+        print(move.vector)
+        print(move.string)
+        print(move.icon)
--- rection1/paddy/machineMovement/machineMovement.py
+++ rection1/paddy/machineMovement/machineMovement.py
@@ -1,19 +1,18 @@
-from ..machineMovement.moveparameter import moveList, moveParameter
-from ..Parameters.paddyParameters import movement
+from ..Parameters.paddyParameters import movement as mv
 from ...util import util
 
 
 # 一番最初に呼び出される
-def goStartPosition(targetPosition, startPosition, insideRowList, insideColumnList):
+def goStartPosition(leftTopPosition, startPosition, insideRowList, insideColumnList):
     # 動きのフラグ、Falseならそこが終点
     moveFlag = True
 
-    oneStepMovementList = []
+    movementList = []
 
     # 上なら負, 下なら正, 上下しないなら0
-    up = targetPosition[0] - startPosition[0]
+    up = leftTopPosition[0] - startPosition[0]
     # 左なら負, 右なら正, 左右しないなら0
-    left = targetPosition[1] - startPosition[1]
+    left = leftTopPosition[1] - startPosition[1]
 
     nowRowPosition = startPosition[0]
     nowColumnPosition = startPosition[1]
@@ -21,39 +20,22 @@ def goStartPosition(targetPosition, startPosition, insideRowList, insideColumnLi
     print("行", up, "列", left)
 
     while moveFlag:
-        move = movement(up, left)
         print("現在の行", nowRowPosition, "列", nowColumnPosition)
-        print("目標の行", targetPosition[0], "列", targetPosition[1])
-        if nowRowPosition == targetPosition[0] and nowColumnPosition == targetPosition[1]:
+        print("目標の行", leftTopPosition[0], "列", leftTopPosition[1])
+        if nowRowPosition == leftTopPosition[0] and nowColumnPosition == leftTopPosition[1]:
             moveFlag = False
             print("目標到達")
         else:
-            vector = move.vector
-            string = move.string
-            icon = move.icon
-
+            move = mv(up, left).move
             rowVector = -1 * move.vector[0]
             columnVector = -1 * move.vector[1]
-
             up += rowVector
             left += columnVector
-
             nowRowPosition += move.vector[0]
             nowColumnPosition += move.vector[1]
-
             if util.isPositionInsidePolygon(insideRowList, insideColumnList, nowColumnPosition, nowRowPosition):
-                oneStepMovementList.append(moveParameter(vector, string, icon))
+                movementList.append(move)
             else:
                 print("polygonの中ではなくなった")
                 pass
-            print("中身を表示")
-            for i in oneStepMovementList:
-                print(i.vector)
-                print(i.icon)
-                print(i.string)
-    tempMoveList = moveList(oneStepMovementList)
-    return tempMoveList, (nowRowPosition, nowColumnPosition)
-
-
-
-
+    return movementList, (nowRowPosition, nowColumnPosition)
